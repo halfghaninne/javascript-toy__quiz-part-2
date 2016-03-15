a1764818fd7a678cb7af507eafe0fe69c172b019
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-03-15 11:17:26 -0500

    adds code comments

--- front-end/global.js
+++ front-end/global.js
@@ -1,5 +1,7 @@
 window.onload = function(){
 
+  ///////////////////////// VARIABLES /////////////////////////
+
   var submit = document.getElementById("submitter");
   var questionDiv = document.getElementById("question");
   var choicesDiv = document.getElementById("choices");
@@ -12,13 +14,19 @@ window.onload = function(){
   var userTotal = 0;
   var questionsTotal = getQuestionCount();
 
-  // gets number of questions from database
+  /////////////////////////////////////////////////////////////
+
+  ///////////////////////// FUNCTIONS /////////////////////////
+
   function getQuestionCount() {
     var request = new XMLHttpRequest;
+
     request.open("GET", "http://localhost:9292/questions");
+
     request.addEventListener("load", function(event) {
       questionsTotal = event.target.responseText;
-    }); //
+    }); //ends function block, ends EL args, ends EL
+
     request.send();
   }; // ends getQuestionCount function def
 
@@ -26,32 +34,32 @@ window.onload = function(){
   function getQuestionPrompt() {
     var request = new XMLHttpRequest;
     var path = "http://localhost:9292/question/"+questionTracker;
-    // String interpolation was giving me headaches here. How can I do that instead of concatenation?
+    // String interpolation was giving me headaches here. Is concatenation the best/only way?
+
     request.open("GET", path);
 
     request.addEventListener("load", function(event) {
-      // formatting issue; leave it for now.
        questionDiv.innerText = event.target.responseText;
     }); //ends function block, ends EL args, ends EL
 
-    request.send();
-      
+    request.send();  
   }; // ends getQuestionPrompt function definition
+
 
   function getQuestionAnswers() {
     var request = new XMLHttpRequest;
     var path = "http://localhost:9292/question/"+questionTracker+"/answers";
-    
+
     request.open("GET", path);
 
     request.addEventListener("load", function(event) {
        choicesDiv.innerHTML = event.target.responseText;
-       debugger;
     }); //ends function block, ends EL args, ends EL
 
     request.send();
       
   }; // ends getQuestionPrompt function definition
+
 
   function compareAnswer() {
     var request = new XMLHttpRequest;
@@ -61,37 +69,34 @@ window.onload = function(){
 
     request.addEventListener("load", function(event) {
       questionResultDiv.innerHTML = event.target.responseText;
+
       if (event.target.responseText == 'Success!') {
         userTotal ++;
-      };
+      }; // ends if block
+
       userTotalDiv.innerHTML = userTotal+" of "+questionsTotal+" questions right!"
-    }); //ends function block, ends EL args, ends EL
+    }); // ends function block, ends EL args, ends EL
 
     request.send();
   }; //ends compareAnswer function
 
-  getQuestionPrompt();
-  getQuestionAnswers();
-
-
-   // fired when answerRequest loads
+  // fired when answerRequest loads
   function moveForward() {
     userAnswer.value = "";
     questionResultDiv.innerHTML = "";
     questionTracker++;
     getQuestionPrompt();
-  };
+  }; // ends moveForward function
+
+  /////////////////////////////////////////////////////////////
+
+  ///////////////////////// GAME PLAY /////////////////////////
+
+  getQuestionPrompt();
+  getQuestionAnswers();
 
   submit.addEventListener("click", compareAnswer);
 
   next.addEventListener("click", moveForward);
 
-
-
-
- 
-  // //test function
-  // function someFunction() {
-  //   alert("clicked!");
-  // };
-}; // end windowonload.
+  /////////////////////////////////////////////////////////////
